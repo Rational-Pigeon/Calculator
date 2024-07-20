4d2f55efeb4f9d20123b39167660265aba29723d
Rational-Pigeon <y.nematzade.74@gmail.com>
2024-07-20 16:59:18 +0330

    Implemented basic calculator functionality Made a few structural changes to the html layout without chanign any of the visual elements for easier integration with js.

--- script.js
+++ script.js
@@ -1,18 +1,99 @@
-const buttons = document.querySelectorAll(".btn");
-console.table(buttons);
+let num1 = "";
+let operation;
+let num2 = "";
+let display = "0";
+const keys = document.querySelectorAll(".btn");
+const buttons = document.getElementById("calculator");
+const displayScreen = document.getElementById("display");
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        button.style.borderTopColor = "#f6c2f3";
-        button.style.borderLeftColor = "#f6c2f3";
-        button.style.borderRightWidth = "2px";
-        button.style.borderBottomWidth = "2px";
+function operate(num1, operation, num2) {
+    num1 = +num1;
+    num2 = +num2;
+    switch (operation) {
+        case "+":
+            return (num1 + num2).toString();
+        case "-":
+            return (num1 - num2).toString();
+        case "×":
+            return (num1 * num2).toString();
+        case "÷":
+            return (num1 / num2).toString();
+    }
+}
+
+function updateNumbers(event) {
+    if (operation) {
+        if (num2.length <= 10) {
+            num2 += event.target.textContent;
+            display = num2;
+        }
+    }
+    else {
+        if (num1.length <= 10) {
+            num1 += event.target.textContent;
+            display = num1;
+        }
+    }
+
+}
+
+function updateDisplay() {
+    //remove leading zeroes
+    display = display.replace(/^0+/, '');
+    displayScreen.textContent = display;
+}
+
+function update(event) {
+    if (event.target.classList.contains("num")) {
+        updateNumbers(event);
+    }
+    else if (event.target.classList.contains("op")) {
+        if (!operation) {
+            operation = event.target.textContent;
+
+            // this nested if, puts the result of previous operation as num1
+            if (!num1 && !(display === "0")) {
+                num1 = display;
+            }
+        }
+        else {
+            display = operate(num1, operation, num2);
+            num1 = display;
+            operation = event.target.textContent;
+            num2 = "";
+        }
+    }
+    else if (event.target.classList.contains("eq")) {
+        if (operation) {
+            display = operate(num1, operation, num2);
+            num1 = "";
+            num2 = "";
+            operation = null;
+        }
+        else { return }
+    }
+    updateDisplay();
+}
+
+buttons.addEventListener("click", update)
+
+
+
+
+
+// button interactivity
+keys.forEach(key => {
+    key.addEventListener("click", () => {
+        key.style.borderTopColor = "#f6c2f3";
+        key.style.borderLeftColor = "#f6c2f3";
+        key.style.borderRightWidth = "2px";
+        key.style.borderBottomWidth = "2px";
 
         setTimeout(() => {
-            button.style.borderTopColor = "";
-            button.style.borderLeftColor = "";
-            button.style.borderRightWidth = "";
-            button.style.borderBottomWidth = "";
+            key.style.borderTopColor = "";
+            key.style.borderLeftColor = "";
+            key.style.borderRightWidth = "";
+            key.style.borderBottomWidth = "";
         }, 150)
     });
 });
